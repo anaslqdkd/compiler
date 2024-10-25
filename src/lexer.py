bcd6ee5923fbf2c48876f235fc77c8f9644c837e
SALHI Amine <amine.salhi@telecomnancy.eu>
2024-10-25 18:10:29 +0200

    Lexer: tokenize

--- src/lexer.py
+++ src/lexer.py
@@ -42,3 +42,37 @@ class Lexer:
 
         self.keywords = {"if", "else", "and", "or", "not", "True", "False", "None", "def", "return", "print", "for", "in"}
         self.symbols = {"(", ")", "{", "}", "[", "]", ":", ","}
+
+    def tokenize(self):
+        while self.position < len(self.source_code):
+            char = self.source_code[self.position]
+            
+            if char == " " or char == "\t":
+                self.position += 1
+            elif char == "#":
+                self.skip_comment()
+            elif char == "\n":
+                self.advance()
+                self.tokens.append(Token(TokenType.NEWLINE, None, self.line_number))
+                self.line_number += 1
+                self.handle_indentation()
+            elif char.isalpha() or char == "_":
+                self.tokens.append(self.process_identifier())
+            elif char.isdigit():
+                self.tokens.append(self.process_integer())
+            elif char == '"':
+                self.tokens.append(self.process_string())
+            elif char in {"+", "-", "*", "/", "%", "<", ">", "=", "!"}:
+                self.tokens.append(self.process_operator())
+            elif char in self.symbols:
+                self.tokens.append(Token(TokenType.SYMBOL, char, self.line_number))
+                self.advance()
+            else:
+                raise SyntaxError(f"Unexpected character '{char}' at line {self.line_number}")
+
+        while len(self.indent_stack) > 1:
+            self.indent_stack.pop()
+            self.tokens.append(Token(TokenType.DEDENT, self.indent_stack[-1], self.line_number))
+        
+        self.tokens.append(Token(TokenType.EOF, None, self.line_number))
+        return self.tokens
