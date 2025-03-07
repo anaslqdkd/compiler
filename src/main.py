bebe23403caa74b952f79ffb11865cad80792321
Amine Salhi <amine.salhi@telecomnancy.eu>
2025-03-07 20:34:19 +0100

    dfs type check

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from lexer import *
 from parser import *
+# from st_builder import init_st, print_all_symbol_tables
+from semantic_analyzer import process_ast
 
 
 def main():
@@ -7,38 +9,13 @@ def main():
         source_code = file.read()
 
     lexer = Lexer(source_code)
+    parser = Parser(lexer, False)
+    parser.parse()
+    transform_to_ast(parser.root)
+    # type_errors(parser.root)
+    # NOTE: mis ici pour tester, à enlever avant de mettre sur main
 
-    while True:
-        token = lexer.get_next_token()
-
-        if token.value:
-            print((token.number, token.value), TokenType.lexicon[token.number])
-        else:
-            print(TokenType.lexicon[token.number])
-
-        if token.number == 4:  # EOF token
-            break
-
-    # parser = Parser(lexer, True)
-    # # parser.parse_s()
-    # print(parser.parse_s())
-    # transform_to_ast(parser.root)
-    # parser.root.get_flowchart(file_path="./test.txt", print_result=False)
-
-    # print("\nToken lexicon", TokenType.lexicon)
-    # print("\nIdentifier Lexicon:\n", lexer.identifier_lexicon)
-    # print("\nConstant Lexicon:\n", lexer.constant_lexicon)
-
-    # print("\nTest Lookahead:")
-    # lexer = Lexer(source_code)
-    # token1 = lexer.get_next_token()
-    # token2 = lexer.peek_next_token()
-    # token3 = lexer.get_next_token()
-
-    # print("Token 1:", token1)
-    # print("Token 2 (peek):", token2)
-    # print("Token 3:", token3)
-
+    print(process_ast(parser.root, lexer.identifier_lexicon))
 
 if __name__ == "__main__":
     main()
---
+++ src/semantic_analyzer.py
@@ -0,0 +1,94 @@
+from tree_struct import Tree
+from parser import *
+from lexer import *
+
+class SemanticError(Exception):
+    pass
+
+def dfs_type_check(node):
+    """
+    Parcours récursif de l'AST pour vérifier le typage et renvoyer le type de l'expression.
+    """
+    if not node.children:
+        return TokenType.lexicon[node.data]
+
+    if TokenType.lexicon[node.data] in ['+', '-', '*', '//', '%', '<', '>']:
+        left_type = dfs_type_check(node.children[0])
+        right_type = dfs_type_check(node.children[1])
+        if left_type != right_type:
+            raise SemanticError(f"Erreur de typage : impossible de faire l'opération entre {left_type} et {right_type}")
+        return left_type
+
+    for child in node.children:
+        dfs_type_check(child)
+    return None 
+
+def process_ast(root, identifier_lexicon):
+    for statement in root.children:
+        var_name = identifier_lexicon[statement.children[0].value]
+        try:
+            expr_type = dfs_type_check(statement.children[1])
+            print(f"Variable '{var_name}' typée en {expr_type}")
+        except SemanticError as e:
+            print(f"Erreur sur la déclaration de {var_name} : {e}")
+
+
+class SemanticError(Exception):
+    pass
+
+def dfs_type_check(node):
+    """
+    Parcours récursif de l'AST pour vérifier le typage et renvoyer le type de l'expression.
+    """
+    if not node.children:
+        return TokenType.lexicon[node.data]
+
+    if TokenType.lexicon[node.data] in ['+', '-', '*', '//', '%', '<', '>']:
+        print(node.data)
+        left_type = dfs_type_check(node.children[0])
+        right_type = dfs_type_check(node.children[1])
+        if left_type != right_type:
+            raise SemanticError(f"Erreur de typage : impossible de faire l'opération entre {left_type} et {right_type}")
+        return left_type
+
+    for child in node.children:
+        dfs_type_check(child)
+    return None 
+
+def process_ast(root, identifier_lexicon):
+    for statement in root.children:
+        var_name = identifier_lexicon[statement.children[0].value]
+        try:
+            expr_type = dfs_type_check(statement.children[1])
+            print(f"Variable '{var_name}' typée en {expr_type}")
+        except SemanticError as e:
+            print(f"Erreur sur la déclaration de {var_name} : {e}")
+
+
+
+def type_errors(given_tree: "Tree") -> None:
+    # TODO: fonction qui vérifie les types pour les relations binaires
+    i = 0
+    relation_symbols = ["*", "+", "//", "%"]
+    while i < len(given_tree.children):
+        child = given_tree.children[i]
+        print(child.is_terminal, child.data)
+        # type_errors(child)
+        # if (child.data in TokenType.lexicon.keys() and TokenType.lexicon[child.data] in relation_symbols):
+        #     left_node = child.children[0]
+        #     right_node = child.children[1]
+        #     left_token = TokenType.lexicon[left_node.data]
+        #     right_token = TokenType.lexicon[right_node.data]
+        #     print("left_node", left_token)
+        #     print("right_node", right_token)
+        #     if (TokenType.lexicon[child.data] == "+"):
+        #         if not (
+        #             (left_token == 'INTERGER' and right_token == 'INTEGER') or
+        #             (left_token == 'STRING' and right_token == 'STRING') or
+        #             (left_token == 'INTEGER' and right_token == 'STRING') or
+        #             (left_token == 'STRING' and right_token == 'INTEGER') or
+        #             ('IDENTIFIER' in [left_token, right_token] and left_token in [
+        #              'STRING', 'INTEGER'] and right_token in ['STRING', 'INTEGER'])
+        #         ):
+        #             # NOTE: voir pour vérifier le type de IDENTIFIER mais ça sera apès dans le contrôle dynamique avec la table de symboles mais donc comment vérifier le type si on l'a pas à la compil? idk
+        i += 1
